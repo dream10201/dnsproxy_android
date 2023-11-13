2996623c425b426c1c684dc0052c00815c4fcd7a
Jason Xu <jason.xu@eastech.com>
2023-11-13 10:07:33 +0800

    去除一些限制

--- service.sh
+++ service.sh
@@ -1,4 +1,45 @@
 #!/system_ext/bin/bash
+# Notes:
+## resetprop (without -n) = deletes a property then modifies it, this forces property_service to update that property immediately.
+## Avoid changing props that can be important to a niche group of users, such as "persist.traced.enable".
+
+# 'dun' persistently tells the telecom that tethering was used.
+# Only a reboot can remove the 'dun' APN flag; but here we disable setting 'dun' in the first place.
+resetprop tether_dun_required 0
+
+# Don't tell the telecom to check if tethering is even allowed for your data plan.
+resetprop net.tethering.noprovisioning true
+resetprop tether_entitlement_check_state 0
+
+# Fully shut-down the device to prevent connection issues; never hibernate on "Power off".
+resetprop -p persist.ro.config.hw_quickpoweron false
+resetprop -p persist.ro.warmboot.capability 0
+
+
+#== Performance tweaks ==
+sysctl -w kernel.sched_schedstats=0
+echo off > /proc/sys/kernel/printk_devkmsg
+for disks in /sys/block/*/queue; do
+    # Don't log I/O statistics.
+    echo 0 > "$disks/iostats" 
+done
+# Use "Explicit Congestion Notification" for both incoming and outgoing packets.
+sysctl -w net.ipv4.tcp_ecn=1
+# Consume more battery while semi-idle to have more stable internet.
+## For some devices with old Linux kernels, this lessens CPU interrupts and thus saves battery.
+sysctl -w kernel.timer_migration=0
+
+
+# Use the best available TCP congestion algorithm.
+sysctl -w net.ipv4.tcp_congestion_control=cubic
+sysctl -w net.ipv4.tcp_congestion_control=bbr
+sysctl -w net.ipv4.tcp_congestion_control=bbr2
+sysctl -w net.ipv4.tcp_congestion_control=bbr3
+
+# Don't apply iptables rules until Android has fully booted.
+until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
+    sleep 1s
+done
 
 dumpsys deviceidle whitelist +com.android.networkstack.overlay
 dumpsys deviceidle whitelist +com.android.networkstack
